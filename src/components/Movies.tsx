6365032e210c6616a666ccc9fb7dff933592340c
Zhabskyi <poltavagaz@gmail.com>
2021-07-22 19:36:14 -0600

    trigger circleci to run

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -14,7 +14,7 @@ const Movies = () => {
     setMovies([
       { id: 1, title: "The Shawshank Redemption", runtime: 142 },
       { id: 2, title: "The Godfather", runtime: 175 },
-      { id: 3, title: "The Dark Knight", runtime: 153 }
+      { id: 3, title: "The Dark Knight", runtime: 150 }
     ]);
   }, []);
 
